a723e19db1503bb921525164eae3c89cead0073b
Alexander Bilchuk <alexander.bilchuk@gmail.com>
2017-10-31 12:33:44 +0300

    Add Moxy StrategyType for showMessage

--- app/src/main/java/com/sandrlab/listcase/presentation/repos_list/view/GitReposListView.java
+++ app/src/main/java/com/sandrlab/listcase/presentation/repos_list/view/GitReposListView.java
@@ -17,6 +17,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
 
 import com.arellomobile.mvp.MvpView;
+import com.arellomobile.mvp.viewstate.strategy.SkipStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 import com.sandrlab.listcase.models.GitRepoModel;
 
 import java.util.List;
@@ -33,5 +35,7 @@ public interface GitReposListView extends MvpView {
     void hideSwipeToRefreshLoading();
     void showLoading();
     void hideLoading();
+
+    @StateStrategyType(SkipStrategy.class)
     void showMessage(@StringRes int stringResId);
 }
