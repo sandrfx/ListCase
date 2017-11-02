86d22457a350da913cf9abdf5249cb0163dd6c1c
Alexander Bilchuk <alexander.bilchuk@gmail.com>
2017-11-02 16:07:24 +0300

    Move removeNavigator() from onStop to onPause

--- app/src/main/java/com/sandrlab/listcase/presentation/repos_list/view/GitReposListActivity.java
+++ app/src/main/java/com/sandrlab/listcase/presentation/repos_list/view/GitReposListActivity.java
@@ -82,8 +82,8 @@ public class GitReposListActivity extends MvpAppCompatActivity implements GitRep
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
+    protected void onPause() {
+        super.onPause();
         navigatorHolder.removeNavigator();
     }
 
